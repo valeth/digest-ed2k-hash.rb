65453ba7a89ec0a7f197495d80e5fd16d1f3ad3b
Patrick Auernig <patrick.auernig@mykolab.com>
2017-02-13 00:51:46 +0100

    implement ed2k hash algorithm

--- lib/digest/ed2k.rb
+++ lib/digest/ed2k.rb
@@ -1,7 +1,137 @@
-require "digest/ed2k/version"
+# frozen_string_literal: true
+
+require 'digest'
+require 'openssl'
 
 module Digest
-  module Ed2k
-    # Your code goes here...
-  end
+    class ED2K < Digest::Class
+        CHUNK_SIZE = 9_728_000
+
+        def initialize(data = nil)
+            @md4 = OpenSSL::Digest::MD4.new
+            reset
+            self << data unless data.nil?
+        end
+
+        def io(io)
+            while (buf = io.read(CHUNK_SIZE))
+                self << buf
+            end
+
+            self
+        end
+
+        def file(path)
+            File.open(path) do |file|
+                return io(file)
+            end
+        end
+
+        def reset
+            @buf = ''
+            @md4.reset
+            @finalized = false
+            @small = true
+
+            self
+        end
+
+        def update(data)
+            raise RuntimeError if @finalized
+
+            @buf += data
+            hash
+
+            self
+        end
+
+        alias << update
+
+        def digest(str = nil)
+            if str.nil?
+                finish
+                @md4.digest
+            else
+                reset
+                self << str
+                digest
+            end
+        end
+
+        def digest!
+            dig = digest
+            reset
+
+            dig
+        end
+
+        def hexdigest(str = nil)
+            if str.nil?
+                finish
+                @md4.hexdigest
+            else
+                reset
+                self << str
+                hexdigest
+            end
+        end
+
+        def hexdigest!
+            hex = hexdigest
+            reset
+
+            hex
+        end
+
+        def finish
+            unless @finalized
+                if @small
+                    @md4.reset
+                    @md4 << @buf
+                else
+                    @md4 << OpenSSL::Digest::MD4.digest(@buf)
+                end
+
+                @buf = ''
+                @finalized = true
+            end
+
+            self
+        end
+
+        def inspect
+            if @finalized
+                "#<ed2k hash='#{digest}'>"
+            else
+                '#<ed2k unfinalized>'
+            end
+        end
+
+        class << self
+            def digest(data)
+                new(data).digest
+            end
+
+            def hexdigest(data)
+                new(data).hexdigest
+            end
+
+            def io(io)
+                new.io(io)
+            end
+
+            def file(path)
+                new.file(path)
+            end
+        end
+
+        private
+
+        def hash
+            while @buf.size >= CHUNK_SIZE
+                @small = false
+                @md4 << OpenSSL::Digest::MD4.digest(@buf.slice!(0...CHUNK_SIZE))
+            end
+        end
+    end
 end
--- lib/digest/ed2k/version.rb
+++ lib/digest/ed2k/version.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module Digest
-  module Ed2k
-    VERSION = "0.1.0"
-  end
+    class ED2K < Digest::Class
+        VERSION = '0.1.0'
+    end
 end
