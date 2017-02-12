dda70eb7810705a7dc38b5eba0074f15c9838a11
Patrick Auernig <patrick.auernig@mykolab.com>
2017-02-13 00:52:38 +0100

    update spec

--- spec/digest/ed2k_spec.rb
+++ spec/digest/ed2k_spec.rb
@@ -1,11 +1,18 @@
-require "spec_helper"
+# frozen_string_literal: true
 
-RSpec.describe Digest::Ed2k do
-  it "has a version number" do
-    expect(Digest::Ed2k::VERSION).not_to be nil
-  end
+require 'spec_helper'
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+# verified with rhash (https://github.com/rhash/RHash)
+TESTFILES = {
+    '3d8a6f73694a87143dfec4e402462d0e' => './spec/testfiles/9718500b',
+    '26c8ef4bca129c4a9574a9782c0e8695' => './spec/testfiles/9728000b',
+    '929483f338c061fcef48b1d5cbacfe18' => './spec/testfiles/9737500b'
+}.freeze
+
+describe Digest::ED2K do
+    TESTFILES.each do |hash, path|
+        it "hashes file #{path}" do
+            expect(Digest::ED2K.file(path).hexdigest).to eq(hash)
+        end
+    end
 end
