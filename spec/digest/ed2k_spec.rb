0298b07e5fa718b680622c6c7240a8cc252a9c7d
Patrick Auernig <patrick.auernig@mykolab.com>
2017-02-13 02:11:56 +0100

    add a few more test cases
    * test string hashing
    * test RuntimeError when trying to update a finished hash

--- spec/digest/ed2k_spec.rb
+++ spec/digest/ed2k_spec.rb
@@ -4,9 +4,19 @@ require 'spec_helper'
 
 # verified with rhash (https://github.com/rhash/RHash)
 TESTFILES = {
+
+    '31d6cfe0d16ae931b73c59d7e0c089c0' => './spec/testfiles/0b',
     '3d8a6f73694a87143dfec4e402462d0e' => './spec/testfiles/9718500b',
     '26c8ef4bca129c4a9574a9782c0e8695' => './spec/testfiles/9728000b',
     '929483f338c061fcef48b1d5cbacfe18' => './spec/testfiles/9737500b'
+}.freeze
+
+STRINGS = {
+
+    '31d6cfe0d16ae931b73c59d7e0c089c0' => '',
+    '83ff6980515eb8d749238ec12e54baab' => '.',
+    '6a49c8fd36b5ebb69c605f44f20c9083' => 'the quick brown fox',
+    '19c46ea62015d6ed476300c4dd208c67' => 'jumps over the lazy old dog.'
 }.freeze
 
 describe Digest::ED2K do
@@ -14,5 +24,17 @@ describe Digest::ED2K do
         it "hashes file #{path}" do
             expect(Digest::ED2K.file(path).hexdigest).to eq(hash)
         end
+    end
+
+    STRINGS.each do |hash, str|
+        it "hashes string '#{str}'" do
+            expect(Digest::ED2K.new(str).hexdigest).to eq(hash)
+        end
+    end
+
+    it 'raises RuntimeError if updating when finalized' do
+        d = Digest::ED2K.new('hello')
+        d.finish
+        expect { d << ' world' }.to raise_error(RuntimeError)
     end
 end
